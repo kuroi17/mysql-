a1633d77bce5f563e945f6389001fd40221278d6
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-10-30 14:54:59 +0800

    added cmd tricks commands

---
+++ HowToFindFile.cmd
@@ -0,0 +1,5 @@
+cd \xampp\mysql\bin
+mysql -u root -p
+now your cmd will look like MariaDB [(none)]>
+SHOW DATABASES;
+USE databaseName;
---
+++ HowtoCreateDatabase.cmd
@@ -0,0 +1,7 @@
+cd xampp\mysql\bin // locate first file path
+mysql -u root -p //accessing MySQL/MariaDB using the command line client
+
+
+CREATE DATABASE databaseName;
+USE databaseName;
+
