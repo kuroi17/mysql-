7332a7859d52489224ad2015921a6e6f8897d8b3
Jyvhan Earl Ponce <140694543+kuroi17@users.noreply.github.com>
2025-11-04 15:24:06 +0000

    improved explanation:

--- SecondActivityPractice/Ponce_Company_Queries.sql
+++ SecondActivityPractice/Ponce_Company_Queries.sql
@@ -1,30 +1,53 @@
 -- Ponce_Company_Operations.sql
-
 USE Ponce_Company;
 
--- 4. Employees hired between Jan 1, 2016 and Dec 31, 2016
+-- 1) Employees hired between Jan 1, 2016 and Dec 31, 2016
 SELECT * FROM Employee
 WHERE EmpHiredDate BETWEEN '2016-01-01' AND '2016-12-31';
 
--- 5. Update job description of J104 to Graphic Designer
-UPDATE Job SET JobDescription = 'Graphic Designer' WHERE JobCode = 'J104';
 
--- 6. Employees who work as Programmer, System Analyst, or Database Administrator
-SELECT e.*
-FROM Employee e
-JOIN Job j ON e.JobCode = j.JobCode
-WHERE j.JobDescription IN ('Programmer', 'System Analyst', 'Database Administrator');
+-- 2) Update job description of J104 to Graphic Designer
+UPDATE Job
+SET JobDescription = 'Graphic Designer'
+WHERE JobCode = 'J104';
 
--- 7. Employees whose last name ends with 'S'
-SELECT * FROM Employee WHERE EmpLName LIKE '%S';
 
--- 8. Employees whose first name starts with 'K' and ends with 'N'
-SELECT * FROM Employee WHERE EmpFName LIKE 'K%N';
+-- 3) Retrieve employees who are Programmer, System Analyst, or Database Administrator
 
--- 9. Employees hired after 2016 AND JobCode J102
+-- EXPLANATION:
+-- Employee table contains JobCode only (like J101, J102...)
+-- Job table contains JobDescription (like "Programmer", "System Analyst"...)
+-- To match them, we use JOIN so we can filter by the job description.
+
+SELECT e.*                        -- Get all employee details
+FROM Employee e                   -- e = shortcut (alias) for Employee table
+JOIN Job j                        -- j = alias for Job table
+    ON e.JobCode = j.JobCode      -- This connects Employee to Job based on JobCode
+WHERE j.JobDescription IN (       -- Filter by the job descriptions we want
+    'Programmer',
+    'System Analyst',
+    'Database Administrator'
+);
+
+
+-- 4) Employees whose last name ends with 'S'
 SELECT * FROM Employee
-WHERE EmpHiredDate > '2016-12-31' AND JobCode = 'J102';
+WHERE EmpLName LIKE '%S';
 
--- 10. Employees hired in 2016 OR JobCode J101
+
+-- 5) Employees whose first name starts with 'K' and ends with 'N'
+-- Example: "Kevin", "Keen", "Kern"
 SELECT * FROM Employee
-WHERE YEAR(EmpHiredDate) = 2016 OR JobCode = 'J101';
+WHERE EmpFName LIKE 'K%N';
+
+
+-- 6) Employees hired AFTER 2016 AND have JobCode J102
+SELECT * FROM Employee
+WHERE EmpHiredDate > '2016-12-31'
+AND JobCode = 'J102';
+
+
+-- 7) Employees hired in 2016 OR JobCode is J101
+SELECT * FROM Employee
+WHERE YEAR(EmpHiredDate) = 2016
+OR JobCode = 'J101';
